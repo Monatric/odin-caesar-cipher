fa0682673d1f2f35b204534ad7975bf04cd983f1
Monatric <nawpnawp8@gmail.com>
2024-06-11 00:31:56 +0800

    Add a few key/val pairs for the alphabet

--- script.rb
+++ script.rb
@@ -1,3 +1,14 @@
+downcase_alphabet = {
+    'a' => 1,
+    'b' => 2,
+    'c' => 3,
+    'd' => 4,
+    'e' => 5,
+    'f' => 6,
+    'g' => 7
+}
+p downcase_alphabet['d']
+
 def caesar_cipher(string, shift_factor)
 
 end
