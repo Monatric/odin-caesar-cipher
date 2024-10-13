84280aba59d1c28942b6f791362434672f55bdbf
Monatric <nawpnawp8@gmail.com>
2024-10-13 19:15:19 +0800

    Add tests for #caesar_cipher

--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -1,7 +1,36 @@
 require_relative "../script"
 
 describe "Caesar Cipher" do
-  describe "#alphabet?" do
+  describe "#caesar_cipher" do
+    context "when an all downcase, alphabetical word is passed with 5 shifts" do
+      subject(:hello) { "hello" }
+
+      it "returns 'mjqqt'" do
+        result = caesar_cipher(hello, 5)
+        expect(result).to eq("mjqqt")
+      end
+    end
+
+    context "when a mixed of downcase and upcase, alphabetical word is passed with 5 shifts" do
+      subject(:hello) { "HeLlo" }
+
+      it "returns 'MjQqt'" do
+        result = caesar_cipher(hello, 5)
+        expect(result).to eq("MjQqt")
+      end
+    end
+
+    context "when a mixed of downcase, upcase, alphabet and symbols in word is passed with 5 shifts" do
+      subject(:hello) { "HeLlo!" }
+
+      it "returns 'MjQqt!'" do
+        result = caesar_cipher(hello, 5)
+        expect(result).to eq("MjQqt!")
+      end
+    end
+  end
+
+  describe " # alphabet?" do
     context "when a valid char is passed" do
       subject(:valid_char) { String.new("a") }
       it "returns true" do
